ce2243207065bece30e21c76d6479c09756a003c
oliver <olivermanzi.dev@gmail.com>
2024-07-14 16:36:56 +0000

    patch: adds alias setter in utils

--- scripts/utils/platform.sh
+++ scripts/utils/platform.sh
@@ -3,7 +3,7 @@
 UPDATED=1
 
 # returns the current platform
-function get_platform() {
+get_platform () {
   name=$(uname)
 
   if [ "$name" = "Darwin" ]; then
@@ -16,7 +16,7 @@ function get_platform() {
 }
 
 # returns true if the current platform is macOS
-function is_macos() {
+is_macos () {
   if [ "$(uname)" = "Darwin" ]; then
     return 0
   else
@@ -25,7 +25,7 @@ function is_macos() {
 }
 
 # returns true if the current platform is linux
-function is_linux() {
+is_linux () {
   if [ "$(uname)" = "Linux" ]; then
     return 0
   else
@@ -34,7 +34,7 @@ function is_linux() {
 }
 
 # updates the package manager
-function update_package_manager() {
+update_package_manager () {
   arg=$1
 
   # if arg is equal to 'force', then update the package manager
@@ -53,47 +53,110 @@ function update_package_manager() {
   UPDATED=0
 }
 
-function reset_update_flag() {
+reset_update_flag () {
   UPDATED=1
 }
 
 # installs a package using the appropriate package manager
-#function install_package() {
-#  package=$1
+install_package () {
+  package=$1
 
-#  # update package manager if not already updated
-#  if [ $UPDATED -eq 1 ]; then
-#    update_package_manager
-#  fi
+  echo "Function $0 (install_package) not Implemented"
+  exit 1
 
-#  if is_macos; then
-#    brew install $package
-#  fi
+  # update package manager if not already updated
+  if [ $UPDATED -eq 1 ]; then
+    update_package_manager
+  fi
 
-#  if is_linux; then
-#    sudo apt install $package -y
-#  fi
-#}
+  if is_macos; then
+    brew install $package
+  fi
 
-# returns true if zsh is installed
-function is_zsh_installed() {
-  if [ -n "$(command -v zsh)" ]; then
-    return 0
-  else
+  if is_linux; then
+    sudo apt install $package -y
+  fi
+}
+
+is_alias_set () {
+  rc_path=$1
+  alias=$2
+
+  if [ -z "$rc_path" ]; then
+    echo "No rc file provided"
     return 1
+  fi
+
+  if [ -z "$alias" ]; then
+    echo "No alias provided"
+    return 1
+  fi
+
+  if [ $(alias | grep -c "$alias") -gt 0 ]; then
+    return 0
+  fi
+
+  return 1
+}
+
+# returns the .bashrc or .zshrc file path depending on the current shell
+get_rc_file () {
+  if [ -n "$BASH_VERSION" ]; then
+    echo "$HOME/.bashrc"
+  fi
+
+  if [ -n "$ZSH_VERSION" ]; then
+    echo "$HOME/.zshrc"
   fi
 }
 
 # adds an argument to the bashrc or zshrc file
-function set_run_command() {
+set_alias () {
   arg=$1
 
-  # find if zsh is installed
-  if is_zsh_installed; then
-    echo "$arg" >>~/.zshrc
-    source ~/.zshrc
+  if [ -z "$arg" ]; then
+    echo "No argument provided"
+    return
+  fi
+
+  rc_file=$(get_rc_file)
+  
+  is_alias_set $rc_file $arg
+  alias_already_set=$?
+
+  if [ $alias_already_set == 0 ]; then
+    echo "Alias '$arg' already set in $rc_file"
+    return
+  fi
+
+  if [[ $alias_already_set == 0 ]]; then
+    echo "Alias '$arg' already set in $rc_file"
   else
-    echo "$arg" >>~/.bashrc
-    source ~/.bashrc
+    echo "Setting '$arg' alias in $rc_file"
+    echo "$arg" >> "$rc_file"
+    source "$rc_file"
+  fi
+
+}
+
+unset_alias () {
+  arg=$1
+
+  if [ -z "$arg" ]; then
+    echo "No argument provided"
+    return
+  fi
+
+  rc_file=$(get_rc_file)
+
+  is_alias_set $rc_file $arg
+  alias_already_set=$?
+
+  if [[ $alias_already_set == 0 ]]; then
+    echo "Unsetting '$arg' alias in $rc_file"
+    sed -i "/$arg/d" "$rc_file"
+    source $rc_file
+  else
+    echo "Alias '$arg' not set in $rc_file"
   fi
 }
