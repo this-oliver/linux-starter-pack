49900d7ed18338144f0b37d5a13cc22aba7ffe3a
oliver <olivermanzi.dev@gmail.com>
2024-07-14 16:00:45 +0000

    patch: updates basic setup

--- scripts/setup-basics.sh
+++ scripts/setup-basics.sh
@@ -1,53 +1,53 @@
-#!/usr/bin/env bash
+#!/bin/env bash
 
-# This script installs some common tools that are used in development
-# environments.
-# 
-# Below is a short description of each tool:
-#   - curl: a command line tool and library for transferring data with URLs
-#   - git: a distributed version control system
-#   - nmap: a network exploration tool and security / port scanner
-#   - bat: a cat clone with syntax highlighting and Git integration
+mode=$1
 
 curr_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $curr_dir/utils/platform.sh
 
-utility="Basic tools"
-system=$(get_platform)
-platform_supported=1
+# ======= Constants =======
 
-if is_linux || is_mac; then
-  platform_supported=0
-fi
+_TOOL_NAME="basic tools"
 
-# ==== INSTALLATION
+ALIAS_BATCAT="alias bat=batcat"
 
-if is_linux; then
-  update_package_manager
-  sudo apt install curl git nmap bat -y
-fi
+# ======= Functions =======
 
-if is_mac; then
-  brew install curl git nmap bat
-fi
+usage() {
+  echo "Usage: $0 [install|uninstall]"
+}
 
-# ==== CONFIGURATION
+linux_install() {
 
-if is_linux || is_mac; then
-  # check if bat is renamed to batcat
-  batcat_path=$(which batcat)
+  echo "=========== installing $_TOOL_NAME ==========="
+  sudo apt install -y curl git nmap bat
 
-  # if batcat is found, set alias for batcat
-  if [ "$batcat_path" != "" ]; then
-      set_run_command "alias bat=batcat"
-  fi
-fi
+  echo "=========== setting $_TOOL_NAME aliases ==========="
+  echo "$ALIAS_BATCAT" >> ~/.bashrc
+  echo "Don't forget to run 'source ~/.bashrc' to apply changes"
 
-# ==== FEEDBACK
-if [ $platform_supported -eq 0 ]; then
-  echo "$utility installed successfully"
-fi
+  echo "*=========== $TOOL_NAME installed successfully ==========="
+}
 
-if [ $platform_supported -ne 0 ]; then
-  echo "$utility is not supported by the Setup Scripts on this system ($system)"
+linux_uninstall() {
+  echo "=========== uninstalling $_TOOL_NAME ==========="
+  sudo apt purge -y curl git nmap bat
+
+  echo "=========== removing $_TOOL_NAME aliases ==========="
+  sed -i "/$ALIAS_BATCAT/d" ~/.bashrc
+  echo "Don't forget to run 'source ~/.bashrc' to apply changes"
+
+  echo "*=========== $TOOL_NAME uninstalled successfully ==========="
+}
+
+# ======= Main =======
+
+if [ "$mode" = "install" ] || [ -z "$mode" ]; then
+  linux_install
+elif [ "$mode" = "uninstall" ]; then
+  linux_uninstall
+else
+  echo "Invalid mode: '$mode'"
+  usage
+  exit 1
 fi
